30212fd0bbf7a9a6ce01f531d5b27b8236a591b1
thforax <t.forax@futurasoft.fr>
2018-08-07 15:51:47 +0200

    Add bootstrap file

---
+++ src/Bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Kiara
+ * Sample chat with mvc
+ *
+ * Copyright (c) Thibault Forax
+ *
+ * Licensed under MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ */
+namespace App;
+
+/**
+ * Bootstrap class
+ *
+ * Manage database and check authentification
+ */
+class Bootstrap
+{
+    public function initialize()
+    {
+    }
+}
