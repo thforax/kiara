442575f34301680b2c4c64aaff1d1d0708ccd57a
thforax <t.forax@futurasoft.fr>
2018-08-07 16:12:47 +0200

    Create sample file for controller and view

---
+++ src/Controller/Error.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Kiara
+ * Sample chat with mvc
+ *
+ * Copyright (c) Thibault Forax
+ *
+ * Licensed under MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ */
+namespace App\Controller;
+
+/**
+ * Controller Error
+ */
+class Error extends Framework\Controller
+{
+    /**
+     * Action index
+     *
+     * @since   1.0
+     */
+    public function index()
+    {
+    }
+}
